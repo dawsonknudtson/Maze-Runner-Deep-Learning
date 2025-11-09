9a08988c6d674cd5a53284dd2aa4ac960d8c8bb1
dawson <dawsonknudtson@protonmail.com>
2025-11-08 18:27:07 -0600

    adding role and border logic

---
+++ src/maze_solver/models/border.py
@@ -0,0 +1,23 @@
+from enum import IntFlag, auto
+
+class Border(IntFlag):
+    EMPTY = 0
+    TOP = auto()
+    RIGHT = auto()
+    BOTTOM = auto()
+    LEFT = auto()
+
+    @property
+    def corner(self) -> bool:
+        return self in (self.TOP | self.LEFT, 
+                        self.TOP | self.RIGHT,
+                        self.BOTTOM | self.RIGHT,
+                        self.BOTTOM | self.LEFT)
+    
+    @property
+    def dead_end(self) -> bool:
+        return self.bit_count() == 3
+
+    @property
+    def intersection(self) -> bool:
+        return self.bit_count() < 2
