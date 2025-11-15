516d0f9b2ba0ce6d7f0594489c7954d8c909ffa1
dawson <dawsonknudtson@protonmail.com>
2025-11-15 14:20:37 -0600

    logic for entrance and exit points

--- src/maze_solver/models/maze.py
+++ src/maze_solver/models/maze.py
@@ -3,7 +3,7 @@ from typing import Iterator
 from functools import cached_property
 
 from maze_solver.models.square import Square
-
+from maze_solver.models.role import Role
 # Using a Tuple (immutable) for now to represent the maze, will be changed to a List
 # For an ever-growing and changing maze, like in the maze runner movie
 
@@ -13,6 +13,35 @@ from maze_solver.models.square import Square
 @dataclass(frozen=True)
 class Maze:
     layout: tuple[Square, ...]
+
+    def __post_init__(self) -> None:
+           validate_indices(self)
+           validate_rows_columns(self)
+           validate_entrance(self)
+           validate_exit(self)
+
+
+def validate_indices(maze : Maze) -> None:
+       assert [square.index for square in maze] == list(
+              range(len(maze.squares))
+       ), "Wrong Sq Index"
+
+def validate_rows_columns(maze : Maze) -> None:
+       for y in range(maze.height):
+             for x in range(maze.width):
+                    square = maze[y * maze.width + x]
+                    assert square.row == y, "Wrong Row"
+                    assert square.col == x, "Wrong Col"
+
+def validate_entrance(maze: Maze) -> None:
+       assert 1 == sum(
+              1 for square in maze if square.role is Role.ENTRANCE
+       ), "Must be exactly one entrance"
+
+def validate_exit(maze : Maze) -> None:
+       assert 1 == sum{
+              1 for square in maze if square.role is Role.EXIT
+       }, "Must be exactly one exit"
 
 def __iter__(self) -> Iterator[Square]:
         return iter(self.squares)
@@ -27,4 +56,13 @@ def width(self):
 @cached_property
 def height(self):
        return max(square.row for square in self) + 1
+
+@cached_property
+def entrance(self) -> Square:
+       return next(sq for sqaure in self if sq.role is Role.ENTRANCE)
+
+@cached_property
+def exit(self) -> Square:
+        return next(sq for sq in self if sq.role is Role.EXIT)
+
 
